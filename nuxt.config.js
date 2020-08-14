7a2c328edd10fbcf9951b4050d169dc05ec61565
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-08-14 11:35:33 +0200

    create Express API structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -90,5 +90,12 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
+  },
+
+  /*
+  ** Server Middleware
+  */
+  serverMiddleware: {
+    '/api': '~/api'
   }
 }
