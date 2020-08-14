a4d1a99d1eb467e86ed5d1b428f2f8811e75ba75
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-08-14 14:00:49 +0200

    add MongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,15 +1,31 @@
+// Require packages
 const express = require('express')
+
+// Import MongoDB connection
+const db = require('./db')
+// DB log
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function callback () {
+  console.log('MongoDB Connected...')
+})
+
+// Require API routes
+const order = require('./order/orderRoute')
+const product = require('./product/productRoute')
+const store = require('./store/storeRoute')
+const user = require('./user/userRoute')
 
 // Create express instance
 const app = express()
 
-// Require API routes
-// const users = require('./routes/users')
-// const test = require('./routes/test')
+// Parse JSON
+app.use(express.json())
 
 // Import API Routes
-// app.use(users)
-// app.use(test)
+// app.use(order)
+// app.use(product)
+// app.use(store)
+// app.use(user)
 
 // Export express app
 module.exports = app
